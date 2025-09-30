b8fabc3d4a5ac79597a876a35677ed0f8e09cb25
BlueBlu2 <eng.abdalrhmn.mohamed@gmail.com>
2025-09-30 10:13:37 +0300

    Creating Classes and Objects

---
+++ FundamentalsHRM/Employee.cs
@@ -0,0 +1,64 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace FundamentalsHRM
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+        private const int minimalHoursWorkedUnit = 1;
+        public Employee(string fn, string ln, string em, DateTime bd, double rate)
+        {
+            firstName = fn;
+            lastName = ln;
+            email = em;
+            birthDay = bd;
+            hourlyRate = rate;
+        }
+
+        public Employee(string fn, string ln, string em, DateTime bd) : this(fn, ln, em, bd, 100)
+        {
+            
+        }
+
+        public void PerformWork()
+        {
+            //numberOfHoursWorked++;
+            PerformWork(minimalHoursWorkedUnit);
+            //Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        }
+
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+
+        }
+
+        public double ReciveWage(bool resetHours = true)
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+
+            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work");
+
+            if (resetHours)
+                numberOfHoursWorked = 0;
+
+            return wage;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst Name: \t{firstName}\nLast Name: \t{lastName}\nEmail: \t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n");
+        }
+    }
+}
---
+++ FundamentalsHRM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FundamentalsHRM;
+
+Console.WriteLine("Creating an Employee");
+Console.WriteLine("--------------------\n");
+
+Employee ahmed = new Employee("Ahmed", "Mohamed", "Ahmed@Mohamd.com",DateTime.Parse("12/2/2000"),500);
+ahmed.DisplayEmployeeDetails();
+
+ahmed.PerformWork();
+ahmed.PerformWork();
+ahmed.PerformWork(20);
+ahmed.PerformWork(2);
+ahmed.PerformWork(6);
+
+ahmed.ReciveWage();
+
+Console.WriteLine("\n\nCreating an Employee");
+Console.WriteLine("--------------------\n");
+
+Employee jack = new("Jack", "Luck", "Jack@Luck.com", new DateTime(1999,8,25), 250);
+jack.DisplayEmployeeDetails();
+
+jack.PerformWork(20);
+jack.PerformWork();
+jack.PerformWork();
+
+var jackWage = jack.ReciveWage(true);
+Console.WriteLine($"Jack wage paid (Message from program): {jackWage}");
